a75e4fcfca319c7d9661139e131c79e7368dd9ec
Peter Duncan <sweisedev@proton.me>
2024-06-18 15:26:09 +0100

    add email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,9 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Contact me here, do it now!</p>
+      <p>
+        Contact me <a href="mailto:sweisedev@proton.me">here</a>
+      </p>
     </div>
   );
 }
